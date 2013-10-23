c374d7f1855b1defe08bb3c773b6402734fd6322
Harley-Ren <renhuailin@gmail.com>
2013-10-23 17:40:01 +0800

    Update snippets.pl

--- snippets.pl
+++ snippets.pl
@@ -1,3 +1,14 @@
 #Get current work directory
 use Cwd;
 my $dir = getcwd;
+
+#Format datetime 
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+
+print($year);
+print("\n");
+print($year+1900);
+print("\n");
+printf "%d-%d-%d %d:%d:%d\n",$year + 1900,$mon + 1,$mday,$hour,$min,$sec;
+my $datestring = sprintf "%d-%d-%d %d:%d:%d\n",$year + 1900,$mon + 1,$mday,$hour,$min,$sec;
+print("Data string: $datestring\n")
